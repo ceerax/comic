f7cc60cf260010dbbeb0ebfce77197d6cc58defa
cesar.moreno <cesar.moreno@educar.com.co>
2021-04-18 13:35:19 -0500

    init

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React ,{useEffect, useState }from 'react'
 import './main.scss';
+import swal from 'sweetalert'
 
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faStar}from '@fortawesome/free-solid-svg-icons';
 
  function MAIN() {
+
+    /* const [imges, setimges] = useState([0]); */
+
+    useEffect(()=>{
+        setComic()
+    })
+
     return (
         <div className="col-12">
             <div className="col-12 card mt-5">
@@ -25,14 +31,48 @@ import {faStar}from '@fortawesome/free-solid-svg-icons';
                             <input id="radio5" type="radio" name="start1" value="1"/><label  className="start" for="radio5">★</label>
                         </p>
                     </form>
+                    
                 </div>
-
-               
+                <div className="col-12 d-flex justify-content-center mb-5">
+                    <button onClick={()=>{Alert()}} type="button" class="btn btn-primary">Enviar calificación</button>               
+                    
+                </div>
             </div>
             
         </div>
         
     )
+
+    function Alert() {
+
+        swal({
+            title: "GRACIAS!",
+            text: "Su calificacion fue enviada ",
+            icon: "success",
+          });
+        
+    }
+
+    
+    function setComic() {
+        getComic().then((valores)=>{
+           console.log(valores);
+        }).catch((error)=>{console.log(error)})        
+    }
+
+    async function getComic() {
+        const response = await fetch("http://xkcd.com/info.0.json", {
+          method: 'GET',
+          
+          headers: {               
+                Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
+              'Content-Type': 'application/json', 
+          }
+          
+        })
+       /*  const json = await response.json(); */
+        return response
+      }
     
 }   
 
